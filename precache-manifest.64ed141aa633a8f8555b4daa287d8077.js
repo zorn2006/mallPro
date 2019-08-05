0a2c0f9fc8919040af12b72d23e11d0718439c50
Dmitrii <dmitrii.zolotuhin@gmail.com>
2019-08-05 19:45:11 +0300

    Updates

---
+++ precache-manifest.64ed141aa633a8f8555b4daa287d8077.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/qr-scanner/static/media/persik.4e1ec840.png"
+  },
+  {
+    "revision": "cd6577ee778fc64324c9",
+    "url": "/qr-scanner/static/js/runtime~main.701dd187.js"
+  },
+  {
+    "revision": "3c1feb007354044ad0d7",
+    "url": "/qr-scanner/static/js/main.c3552df7.chunk.js"
+  },
+  {
+    "revision": "f0059003039bfc9fba00",
+    "url": "/qr-scanner/static/js/2.b27f1798.chunk.js"
+  },
+  {
+    "revision": "3c1feb007354044ad0d7",
+    "url": "/qr-scanner/static/css/main.18b11911.chunk.css"
+  },
+  {
+    "revision": "f0059003039bfc9fba00",
+    "url": "/qr-scanner/static/css/2.55861fc6.chunk.css"
+  },
+  {
+    "revision": "f901d1a7b7384285b9b499615e25dacd",
+    "url": "/qr-scanner/index.html"
+  }
+];
