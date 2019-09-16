e976e2751c53a462345b052099b7528a4ebfc2f8
poprosche <50277564+poprosche@users.noreply.github.com>
2019-09-16 15:44:22 +0300

    Update

---
+++ precache-manifest.00e88c8cfd2364ce3bd49ac04365450f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f44108c9805618dfde2f",
+    "url": "/qr-scanner/static/js/main.cc99cb16.chunk.js"
+  },
+  {
+    "revision": "cd6577ee778fc64324c9",
+    "url": "/qr-scanner/static/js/runtime~main.701dd187.js"
+  },
+  {
+    "revision": "31c118a63ac68566de3a",
+    "url": "/qr-scanner/static/css/2.77868488.chunk.css"
+  },
+  {
+    "revision": "31c118a63ac68566de3a",
+    "url": "/qr-scanner/static/js/2.7601e638.chunk.js"
+  },
+  {
+    "revision": "9baac627b6badd8d01a87d925d5054cb",
+    "url": "/qr-scanner/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/qr-scanner/precache-manifest.69e84150fbe4c978b98137cb404cd8f1.js"
+  "/qr-scanner/precache-manifest.00e88c8cfd2364ce3bd49ac04365450f.js"
 );
 
 workbox.clientsClaim();
